0c44aeb13960998e385a4fd471eb4aff635a8851
preetdumir2009 <86602660+preetdumir2009@users.noreply.github.com>
2022-03-29 13:51:44 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,10 +3,16 @@
 
 
 var spaceImg,space;
+var astroImg,astronaut
+var obstacle1Img,obstacle
+var obstacle2Img,obstacle2
+var obstaclesGroup;
 function preload()
 {
     spaceImg=loadImage("spaceBg.jfif")
-
+    astroImg=loadImage("astroImg.png")
+    obstacle1Img=loadImage("obstacle1.png")
+    obstacle2Img=loadImage("obstacle2.png")
 }
 
 function setup()
@@ -15,12 +21,54 @@ function setup()
 space=createSprite(0,100,width,height)
 space.addImage("spaceBg",spaceImg)
 space.scale=4;
+
+astronaut=createSprite(100,350,width,height)
+astronaut.addImage("astroImg",astroImg)
+astronaut.scale=0.75
+
+obstaclesGroup=new Group ()
+
+
 }
 
 
 function draw()
 {
-drawSprites()
+
+if (keyDown("space"))
+{
+    astronaut.velocityY = -16;
 }
 
+astronaut.velocityY=astronaut.velocityY+0.8
+
+drawSprites()
+spawnObstacles()
+}
+
+
+function spawnObstacles() {
+    if(frameCount % 120 === 0) {
+  
+      var obstacle = createSprite(400,330,40,40);
+      obstacle.setCollider("rectangle",0,0,200,200)
+      //obstacle.addImage(obstacle1Img);
+    
+      var rand = Math.round(random(1,2));
+      switch(rand) {
+        case 1: obstacle.addImage(obstacle1Img);
+                break;
+        case 2: obstacle.addImage(obstacle2Img);
+                break;
+        
+        default: break;
+      }
+    obstacle.velocityX = -6;
+            obstacle.scale = 0.2;      
+  
+      obstacle.lifetime = 400;
+      obstaclesGroup.add(obstacle);
+      
+    }
+  }
 
